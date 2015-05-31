30fd12e57accf5ebf88a01668344310d20701d63
furywing <furywing@hanmail.net>
2015-05-31 03:00:09 -0700

    관리자 모드 일부 수정

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -1,8 +1,14 @@
 class AdminController < ApplicationController
   def show
    @users=User.all
+   @user=session[:user_id]
+
   end
 
   def delete_user
+   user=User.find(params[:id])
+   user.destroy
+   flash[:alert]="유저의 계정이 삭제되었습니다."
+   redirect_to "/admin/show"
   end
 end
